ef22a41b074a836c6fc3cbeb0f84a257358922d4
CreatorStefann <stefansandor26@gmail.com>
2025-10-11 22:35:09 +0300

    feat: complete sign-in / logout functionality

--- components/NavItems.tsx
+++ components/NavItems.tsx
@@ -1,13 +1,16 @@
-import {Link, NavLink} from "react-router";
+import {Link, NavLink, useLoaderData, useNavigate} from "react-router";
 import {sidebarItems} from "~/constants";
 import {cn} from "../lib/utils";
+import {logoutUser} from "~/appwrite/auth";
 
 const NavItems = ({handleClick} : {handleClick?: () => void}) => {
-    const user = {
-        name: 'Stefan',
-        email: 'contact@jsmastery.pro',
-        imageUrl: '/assets/images/david.webp',
+    const user = useLoaderData();
+    const navigate = useNavigate();
+    const handleLogout = async () => {
+        await logoutUser();
+        navigate('/sign-in');
     }
+
     return (
         <section className="nav-items">
             <Link to='/' className="link-logo">
@@ -36,7 +39,10 @@ const NavItems = ({handleClick} : {handleClick?: () => void}) => {
                 </nav>
                 
                 <footer className="nav-footer">
-                    <img src={user?.imageUrl || './assets/images.david.webp'} alt={user?.name || 'David'}/>
+                    <img src={user?.imageUrl || './assets/images.david.webp'}
+                         alt={user?.name || 'David'}
+                         referrerPolicy="no-referrer"
+                    />
 
                     <article>
                         <h2>{user?.name}</h2>
@@ -44,9 +50,7 @@ const NavItems = ({handleClick} : {handleClick?: () => void}) => {
                     </article>
 
                     <button
-                        onClick={() => {
-                            console.log('logout');
-                        }}
+                        onClick={handleLogout}
                         className="cursor-pointer"
                     >
                         <img
